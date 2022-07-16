2cd9662710f5a210a951818dd89b046ae92d8447
omega5300 <omegamatrix5351@gmail.com>
2022-07-15 20:13:03 -0500

    version 1.6.0

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,12 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
 import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(), 
-    vueJsx(),
     VitePWA({
       registerType: "autoUpdate",
       manifest: {
